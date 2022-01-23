fca7f446d319665a6554935fbfc8b88bf243585e
Alan Reid <56002246+Alanapiereid@users.noreply.github.com>
2022-01-23 15:57:22 +0100

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -10,41 +10,39 @@ import matplotlib.pyplot as plt
 # b = intercept
 
 class Linear_Regression:
-    def __init__(self):
-        return None
-    
-    def grad_desc(x,y):
-        m = 0
-        b = 0
-        n = len(x)
-        steps = 500
+    def __init__(self, m, b, n, steps, l_rate):
+        self.m = m
+        self.b = b
+        self.n = n
+        self.steps = steps
+        self.l_rate = l_rate
+
+    def grad_desc(self, x,y):
         # make learning rate small!!
-        learning_rate = 0.0001
-        for i in range(steps):
+        for i in range(self.steps):
             plt.clf()
-            y_pred = m * x + b
+            y_pred = self.m * x + self.b
             # get partial derivative for m from cost function
-            m_der = -(2/n) * (sum(x*(y-y_pred)))
+            m_der = -(2/self.n) * (sum(x*(y-y_pred)))
             # get partial derivative for b from cost function
-            b_der = -(2/n) * (sum(y-y_pred))
+            b_der = -(2/self.n) * (sum(y-y_pred))
             # m update
-            m = m - learning_rate * m_der
+            self.m = self.m - self.l_rate * m_der
             # b update
-            b = b - learning_rate * b_der
-            cost = (1/n) * sum([i**2 for i in (y - y_pred)])
-            cost = round(cost, 4)
+            self.b = self.b - self.l_rate * b_der
+            cost = round((1/self.n) * sum([i**2 for i in (y - y_pred)]), 4)
+            #cost = round(cost, 4)
             plt.scatter(x,y)
             plt.plot([min(x), max(y)], [min(y_pred), max(y_pred)], color='green')
             plt.text(7.0, 17.5, f'{cost}', style='italic',
             bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 6})
             plt.pause(0.01)
-        plt.show()
-
+        return plt.show()
 
 if __name__ == '__main__':
     x = random.randint(20, size=(20))
     y = random.randint(20, size=(20))
-    Linear_Regression.grad_desc(x,y)
+    Linear_Regression(0, 0, len(x), 100, 0.0001).grad_desc(x,y)
 
 
 
